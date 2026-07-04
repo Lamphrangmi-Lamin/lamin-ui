e415c2bb6c9a974d1d719d420f1dcb31c5f0547c
Lamphrangmi <lamphrangmilamin46169@gmail.com>
2026-07-04 23:52:59 +0530

    feat: add Button component to the design system

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,54 @@ import Button from "./components/Button/Button"
 
 function App() {
   return (
-    // <div className="flex flex-col gap-6 justify-center items-center h-screen">
-    //   <div className="flex justify-center items-center gap-6">
-    //     <Badge size="sm" color="neutral"/>
-    //     <Badge size="md" color="neutral"/>
-    //     <Badge size="lg" color="neutral"/>
-    //   </div>
+    <div className="flex flex-col gap-12 px-6 md:px-8 py-28">
+      <div className="flex gap-5 items-center flex-wrap">
+        <Button variant="primary" size="md">Button CTA</Button>
+        <Button variant="primary" icon={<i class="ri-star-line"></i>} size="lg">Button CTA</Button>
+        <Button variant="primary" icon={<i class="ri-star-line"></i>} iconPosition="right" size="xl">Button CTA</Button>
+        <Button variant="primary" size="xxl">Button CTA</Button>
+        <Button variant="primary" icon={<i className="ri-star-line"></i>} />
+      </div>
 
-    //   <div className="flex justify-center items-center gap-6">
-    //     <Badge size="sm" color="error"/>
-    //     <Badge size="md" color="error"/>
-    //     <Badge size="lg" color="error"/>
-    //   </div>
+      <div className="flex gap-5 items-center flex-wrap">
+        <Button variant="secondary" size="md" icon={<i class="ri-star-line"></i>}>Button CTA</Button>
+        <Button variant="secondary" size="lg" icon={<i class="ri-star-line"></i>}>Button CTA</Button>
+        <Button variant="secondary" size="xl" icon={<i class="ri-star-line"></i>}>Button CTA</Button>
+        <Button variant="secondary" size="xxl" icon={<i class="ri-star-line"></i>}>Button CTA</Button>
+        <Button variant="secondary" icon={<i className="ri-star-line"></i>} />
+      </div>
 
-    //   <div className="flex justify-center items-center gap-6">
-    //     <Badge size="sm" color="warning"/>
-    //     <Badge size="md" color="warning"/>
-    //     <Badge size="lg" color="warning"/>
-    //   </div>
+      <div className="flex gap-5 items-center flex-wrap">
+        <Button variant="tertiary" size="md" icon={<i class="ri-star-line"></i>}>Button CTA</Button>
+        <Button variant="tertiary" size="lg" icon={<i class="ri-star-line"></i>}>Button CTA</Button>
+        <Button variant="tertiary" size="xl" icon={<i class="ri-star-line"></i>}>Button CTA</Button>
+        <Button variant="tertiary" size="xxl" icon={<i class="ri-star-line"></i>}>Button CTA</Button>
+        <Button variant="tertiary" icon={<i className="ri-star-line"></i>} />
+      </div>
 
-    //   <div className="flex justify-center items-center gap-6">
-    //     <Badge size="sm" color="success"/>
-    //     <Badge size="md" color="success"/>
-    //     <Badge size="lg" color="success"/>
-    //   </div>
+      <div className="flex gap-5 items-center flex-wrap">
+        <Button variant="linkColor" size="md">Button CTA</Button>
+        <Button variant="linkColor" size="lg">Button CTA</Button>
+        <Button variant="linkColor" size="xl">Button CTA</Button>
+        <Button variant="linkColor" size="xxl">Button CTA</Button>
+        <Button variant="linkColor" icon={<i className="ri-star-line"></i>} />
+      </div>
 
-    //   <div className="flex justify-center items-center gap-6">
-    //     <Badge size="sm" color="brand"/>
-    //     <Badge size="md" color="brand"/>
-    //     <Badge size="lg" color="brand"/>
-    //   </div>
-    // </div>
-    <div>
-      <Button buttonText="Explore products" icon=""/>
-      <Button buttonText="Remove item" icon=""/>
+      <div className="flex gap-5 items-center flex-wrap">
+        <Button variant="linkGray" size="md">Button CTA</Button>
+        <Button variant="linkGray" size="lg">Button CTA</Button>
+        <Button variant="linkGray" size="xl">Button CTA</Button>
+        <Button variant="linkGray" size="xxl">Button CTA</Button>
+        <Button variant="linkGray" icon={<i className="ri-star-line"></i>} />
+      </div>
+
+      <div className="flex gap-5 items-center flex-wrap">
+        <Button variant="destructive" size="md">Button CTA</Button>
+        <Button variant="destructive" size="lg">Button CTA</Button>
+        <Button variant="destructive" size="xl">Button CTA</Button>
+        <Button variant="destructive" size="xxl">Button CTA</Button>
+        <Button variant="destructive" icon={<i className="ri-star-line"></i>} />
+      </div>
     </div>
   )
 }
